3b0b9f5117818ae54380dfa5971ddfe89735d0a3
Valerii <valera.dchn@gmail.com>
2024-03-14 00:00:22 +0200

    Refactoring

--- qr-sys/app/API/api/User/methods/GET/__init__.py
+++ qr-sys/app/API/api/User/methods/GET/__init__.py
@@ -54,7 +54,7 @@ async def get_full_info_from_user(hashf: str = Depends(jwt)):
 
 
     data = user.get_parse_data() | {"restaurant": restaurant_data.get_data() 
-                                 |         {"categories": [dict(i) for i in category]}
+                                 |         {"categories": [i.get_data() for i in category]}
                                  |         {"tables_count": table_count[0] if table_count else 0}
                                  }
 
